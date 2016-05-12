e4daad7731896aa3155dd6e08bcc506b65974309
Aclz <12345@67890.ru>
2016-05-13 04:18:49 +0500

    Issue #2 fix, few minor changes

--- python3/app/cron/get_ldap_users.py
+++ python3/app/cron/get_ldap_users.py
@@ -1,13 +1,13 @@
 """
-Получение списка пользователей AD из LDAP с аутентификацией Kerberos
+Getting AD users from LDAP with Kerberos auth
 """
 
+import os
+import ssl
+import ldap3
+
 def get_ldap_users(keytab_file_path, ldap_url, base_dn, ldap_query):
-    import os
-    import ssl
-
-    import ldap3
-
+    #set keytab path for ldap3 kerberos auth to work
     os.environ["KRB5_CLIENT_KTNAME"] = keytab_file_path
 
     tls = ldap3.Tls(validate=ssl.CERT_NONE, version=ssl.PROTOCOL_TLSv1_2)
@@ -15,18 +15,18 @@ def get_ldap_users(keytab_file_path, ldap_url, base_dn, ldap_query):
     connection = ldap3.Connection(server, authentication=ldap3.SASL, sasl_mechanism='GSSAPI')
     connection.bind()
 
+    #get users
     connection.search(search_base=base_dn, search_filter=ldap_query, search_scope=ldap3.SUBTREE,
         attributes=['cn', 'userPrincipalName'])
 
     ad_search = connection.response
 
-    #Отключаемся от LDAP
+    #disconnect from LDAP
     connection.unbind()
 
-    #Dictionary пользователей
     users_dict = {}
 
-    #Заполняем наш ассоциативный массив
+    #fill the users dict
     for element in ad_search:
         if not element['dn']:
             continue
@@ -34,14 +34,6 @@ def get_ldap_users(keytab_file_path, ldap_url, base_dn, ldap_query):
         if not element['dn'] in users_dict:
             users_dict[element['dn']] = (element['attributes']['cn'], element['attributes']['userPrincipalName'])
 
-    #Превратим dictionary в list of tuples для удобства дальнейшей вставки в MySQL
+    #make a list of tuples for further convenience
     return [(dn[dn.upper().find(',OU=') + 1:dn.upper().find(',' + base_dn.upper())], users_dict[dn][0], users_dict[dn][1])
         for dn in users_dict]
-
-
-def main():
-    print("This module is not intended to be used directly!")
-
-
-if __name__ == '__main__':
-    main()
